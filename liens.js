be6fdfe8a82b4fb15cec2eaba11d4198fc84b4b0
ObscureAngel <Obscure-Angel@gmx.fr>
2022-07-13 22:04:18 +0200

    Changements des liens de xml à json

---
+++ liens.js
@@ -0,0 +1,376 @@
+var lo_jsonData = {
+	"categories" : [
+		{
+			"titre": "Jeu vid&eacute;o",
+			"ligne": "0",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://www.qwant.com",
+					"titre": "Instant Gaming",
+					"description": "Moteur de recherche"
+				},
+				{
+					"ligne": "1",
+					"url": "https://www.qwant.com",
+					"titre": "Steam",
+					"description": "Moteur de recherche"
+				},
+				{
+					"ligne": "1",
+					"url": "https://www.qwant.com",
+					"titre": "Ubisoft",
+					"description": "Moteur de recherche"
+				},
+				{
+					"ligne": "1",
+					"url": "https://www.qwant.com",
+					"titre": "Epic Games",
+					"description": "Moteur de recherche"
+				},
+				{
+					"ligne": "2",
+					"url": "https://www.qwant.com",
+					"titre": "Origin",
+					"description": "Moteur de recherche"
+				},
+				{
+					"ligne": "2",
+					"url": "https://www.qwant.com",
+					"titre": "GOG",
+					"description": "Moteur de recherche"
+				}
+			]
+		},
+		{
+			"titre": "Animes",
+			"ligne": "0",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Kanojo%2C+Okarishimasu+vostfr",
+					"titre": "Kanojo Okarishimasu",
+					"description": "Une description"
+				},
+				{
+					"ligne": "0",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Engage+Kiss+vostfr",
+					"titre": "Engage Kiss",
+					"description": "Une description"
+				},
+				{
+					"ligne": "1",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Youkoso+Jitsuryoku+Shijou+vostfr",
+					"titre": "Youkoso Jitsuryoku Shijou",
+					"description": "Une description"
+				},
+				{
+					"ligne": "1",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Isekai+Meikyuu+vostfr",
+					"titre": "Isekai Meikyuu de Harem wo",
+					"description": "Une description"
+				},
+				{
+					"ligne": "2",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Yofukashi+no+Uta+vostfr",
+					"titre": "Yofukashi no Uta",
+					"description": "Une description"
+				},
+				{
+					"ligne": "2",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Kinsou+no+Vermeil+vostfr",
+					"titre": "Kinsou no Vermeil",
+					"description": "Une description"
+				},
+				{
+					"ligne": "3",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Tensei+Kenja+no+Isekai+Life+vostfr",
+					"titre": "Tensei Kenja no Isekai Life",
+					"description": "Une description"
+				},
+				{
+					"ligne": "4",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Tensei+Kenja+no+Isekai+Life+vostfr",
+					"titre": "MAL",
+					"description": "Une description"
+				},
+				{
+					"ligne": "4",
+					"url": "https://nyaa.si/?f=0&c=0_0&q=Tensei+Kenja+no+Isekai+Life+vostfr",
+					"titre": "MAL",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Films",
+			"ligne": "0",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Livres",
+			"ligne": "0",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://livre.fnac.com/a163187/Michel-Foucault-Surveiller-et-punir",
+					"titre": "Surveiller et punir",
+					"description": "Livre de Michel Foucault"
+				},
+				{
+					"ligne": "0",
+					"url": "https://livre.fnac.com/a15930291/Charles-Prats-Cartel-des-fraudes",
+					"titre": "Cartel des fraudes 2",
+					"description": "Livre de Charles Prats"
+				},
+				{
+					"ligne": "1",
+					"url": "https://livre.fnac.com/a16291175/Marc-Payet-Le-Ministere-des-bras-casses",
+					"titre": "Le Minist&egrave;re des bras cass&eacute;s",
+					"description": "Livre de Marc Payet"
+				},
+				{
+					"ligne": "1",
+					"url": "https://www.fnac.com/ia61926/Jean-Paul-Brighelli",
+					"titre": "Deux livres &agrave; prendre ici",
+					"description": "Livre de Marc Payet"
+				}
+			]
+		},
+		{
+			"titre": "D&eacute;veloppement",
+			"ligne": "1",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "PHP/SQL",
+			"ligne": "1",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://intelephense.com/",
+					"titre": "Intelephense",
+					"description": "Plus de productivit&eacute;"
+				}
+			]
+		},
+		{
+			"titre": "JS",
+			"ligne": "1",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Autres langages",
+			"ligne": "1",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Banque/Finances",
+			"ligne": "2",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Imp&ocirc;ts/Prestations familiales",
+			"ligne": "2",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Sant&eacute;",
+			"ligne": "2",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Cuisine",
+			"ligne": "3",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://www.greenpeace.fr/guetteur/calendrier/",
+					"titre": "Guide des saisons des fruits et l&eacute;gumes",
+					"description": "C'est mieux pour savoir quand acheter ses fruits et l&eacute;gumes"
+				},
+				{
+					"ligne": "0",
+					"url": "https://www.marmiton.org/recettes/recette_ail-confit_337637.aspx",
+					"titre": "Recette ail confit",
+					"description": "Pour faire de l'ail confit maison"
+				}
+			]
+		},
+		{
+			"titre": "Ameublement",
+			"ligne": "3",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://www.ikea.com/fr/fr/p/uppspel-pietement-assis-deb-pr-plateau-elec-noir-30511349/",
+					"titre": "Pi&egrave;tement de bureau",
+					"description": "Un bureau assis/debout"
+				},
+				{
+					"ligne": "0",
+					"url": "https://www.ikea.com/fr/fr/p/bekant-bureau-assis-debout-linoleum-bleu-noir-s69281069/",
+					"titre": "Bureau assis/debout",
+					"description": "Pi&egrave;tement et plateau"
+				}
+			]
+		},
+		{
+			"titre": "Hardware",
+			"ligne": "3",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Services",
+			"ligne": "3",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Transport",
+			"ligne": "4",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "Activit&eacute;s ext&eacute;rieures",
+			"ligne": "4",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Lien",
+					"description": "Une description"
+				}
+			]
+		},
+		{
+			"titre": "R&eacute;seaux sociaux",
+			"ligne": "4",
+			"liens": [
+				{
+					"ligne": "0",
+					"url": "https://twitter.com/",
+					"titre": "Twitter",
+					"description": "Une description"
+				},
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Discord",
+					"description": "Une description"
+				},
+				{
+					"ligne": "0",
+					"url": "#",
+					"titre": "Instagram",
+					"description": "Une description"
+				},
+				{
+					"ligne": "1",
+					"url": "https://www.youtube.com/",
+					"titre": "Youtube",
+					"description": "Une description"
+				},
+				{
+					"ligne": "1",
+					"url": "#",
+					"titre": "LinkedIn",
+					"description": "Une description"
+				},
+				{
+					"ligne": "1",
+					"url": "#",
+					"titre": "Facebook",
+					"description": "Une description"
+				},
+				{
+					"ligne": "2",
+					"url": "https://www.reddit.com/",
+					"titre": "Reddit",
+					"description": "Une description"
+				},
+				{
+					"ligne": "2",
+					"url": "#",
+					"titre": "Blablacar",
+					"description": "Une description"
+				},
+				{
+					"ligne": "2",
+					"url": "#",
+					"titre": "Gettr",
+					"description": "Une description"
+				}
+			]
+		}
+	]
+}
